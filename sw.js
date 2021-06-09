6fca4c03c2a286d3e8755b4953d224d8117c912d
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2021-06-09 16:19:54 -0500

    Completed service worker

--- sw.js
+++ sw.js
@@ -8,10 +8,11 @@ const CACHE_ELEMENTS = [
     "./components/Contador.js"
 ]; 
 
-const CACHE_NAME = "v1_cache_contador_react";
+const CACHE_NAME = "v3_cache_contador_react";
 
 /*Evento del sw */
 //self = this;
+/*Instalacion del service worker */
 self.addEventListener("install", (e)=>{
     console.log(e);
     //Espera a que se ejecute lo de dentro
@@ -26,3 +27,28 @@ self.addEventListener("install", (e)=>{
         })
     );
 }); 
+
+/*Activacion del service worker */
+self.addEventListener("activate", (e)=>{
+    const cacheWhiteList = [CACHE_NAME];
+    //Metodo keys da las claves de los caches si hay varios
+    e.waitUntil(
+        caches.keys()
+         .then(cachesNames => {
+             //console.log(cachesNames);
+             return Promise.all(cachesNames.map(cacheName =>{
+                 cacheWhiteList.indexOf(cacheName) === -1 && caches.delete(cacheName);
+             }));
+         })
+         .then(() =>{self.clients.claim()})
+    );
+}); 
+
+/*Fetch del service worker */
+self.addEventListener("fetch", (e)=>{
+    e.respondWith(() =>{
+        //console.log(e.request);
+        caches.match(e.request)
+          .then((response)=>{response ? response : fetch(e.request)});        
+    });
+});
